35e965e20595c5f7ec75a31c8673cd054740211a
Raymond-ap <apunguray@gmail.com>
2021-10-21 12:46:24 -0700

    first commit

---
+++ Router/Product/product_router.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const productController = require('../../Controllers/Product/product_controllers')
+
+const router = express.Router()
+
+// Retrieve all product from DB Route
+router.get('/', productController.getProducts)
+
+router.get('/categories', productController.getProductsCategories)
+
+router.post('/', productController.createProduct)
+
+// Route to update existing product
+router.patch('/:pid', productController.updateProuctById)
+
+// Route to delete existing product
+router.delete('/:pid', productController.deleteProductById)
+
+// Route to retrieve product by ID
+router.get('/id?=:pid', productController.getProductById)
+
+// Route to retrieve product by Category
+router.get('/category?=:category', productController.getProductsByCategory)
+
+// Route to retrieve product by Title
+router.get('/title?=:title', productController.getProductsByTitle)
+
+
+
+module.exports = router
