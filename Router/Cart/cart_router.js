d23b9ff240f5d9397bdd1507abda12c20b575b17
Raymond-ap <apunguray@gmail.com>
2021-10-22 01:07:58 -0700

    added comments

--- Router/Cart/cart_router.js
+++ Router/Cart/cart_router.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const cartController = require('../../Controllers/Cart/cart_controller')
+const express = require("express");
+const cartController = require("../../Controllers/Cart/cart_controller");
 
-const router = express.Router()
+const router = express.Router();
 
+// GET ALL CAART ITEMS ROUTE
+router.get("/", cartController.getCartItems);
 
-router.get('/', cartController.getCartItems)
+// GET CART ITEM BY USER ID ROUTE
+router.get("/:uid", cartController.getCartItemsByUserId);
 
-router.get('/:uid', cartController.getCartItemsByUserId)
+// CREATE NEW CART ITEM ROUTE
+router.post("/", cartController.createCartItem);
 
-router.post('/', cartController.createCartItem)
-
-
-
-module.exports = router
+module.exports = router;
