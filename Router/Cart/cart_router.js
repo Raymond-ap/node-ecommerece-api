8288e8213e850337ccc13c56ace83a16b0b22a0e
Raymond-ap <apunguray@gmail.com>
2021-10-22 01:06:22 -0700

    post route to create new cart

--- Router/Cart/cart_router.js
+++ Router/Cart/cart_router.js
@@ -3,9 +3,13 @@ const cartController = require('../../Controllers/Cart/cart_controller')
 
 const router = express.Router()
 
+
 router.get('/', cartController.getCartItems)
 
 router.get('/:uid', cartController.getCartItemsByUserId)
+
+router.post('/', cartController.createCartItem)
+
 
 
 module.exports = router
